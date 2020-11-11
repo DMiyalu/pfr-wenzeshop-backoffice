13c82655bc06e2bcc6b05dd8524dbed122d3b052
DMiyalu <dieudonnemiyalu6@gmail.com>
2020-11-11 16:16:14 +0100

    table

--- src/components/screens/commande/index.js
+++ src/components/screens/commande/index.js
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import Header from '../../header'
+import Table from '../../table'
+import './style.css'
+
 
 const Commande = () => {
     return (
         <div>
             <Header title="Commande"/>
+            <div className="table-box">
+                <Table/>
+            </div>
         </div>
     )
 }
